6099f41a5ac82f2205431cc3ef47d248d56cef6b
shovk <shovkyntimofej@gmail.com>
2024-07-31 00:23:33 +0300

    init

---
+++ src/main/java/com/javarush/rest/service/ClientService.java
@@ -0,0 +1,19 @@
+package com.javarush.rest.service;
+
+import com.javarush.rest.model.Client;
+
+import java.util.List;
+
+public interface ClientService {
+
+    void create(Client client);
+
+    List<Client> readAll();
+
+    Client read(int id);
+
+    boolean update(Client client, int id);
+
+    boolean delete(int id);
+
+}
