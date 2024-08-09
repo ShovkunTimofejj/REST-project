2711d5659c84cc510999a79869792332e7851c1a
shovk <shovkyntimofej@gmail.com>
2024-08-09 22:03:01 +0300

    add logger

--- src/main/java/com/javarush/rest/service/ClientServiceImpl.java
+++ src/main/java/com/javarush/rest/service/ClientServiceImpl.java
@@ -2,7 +2,8 @@ package com.javarush.rest.service;
 
 import com.javarush.rest.model.Client;
 import org.springframework.stereotype.Service;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +12,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class ClientServiceImpl implements ClientService {
+
+    private static final Logger log = LoggerFactory.getLogger(ClientServiceImpl.class);
 
     private static final Map<Integer, Client> CLIENT_REPOSITORY_MAP = new HashMap<>();
 
@@ -22,15 +25,18 @@ public class ClientServiceImpl implements ClientService {
         final int clientId = CLIENT_ID_HOLDER.incrementAndGet();
         client.setId(clientId);
         CLIENT_REPOSITORY_MAP.put(clientId,client);
+        log.info("Create new client with id {}", clientId);
     }
 
     @Override
     public List<Client> readAll() {
+        log.debug("Reading all clients");
         return new ArrayList<>(CLIENT_REPOSITORY_MAP.values());
     }
 
     @Override
     public Client read(int id) {
+        log.debug("Reading client with id {}", id);
         return CLIENT_REPOSITORY_MAP.get(id);
     }
 
@@ -39,6 +45,7 @@ public class ClientServiceImpl implements ClientService {
         if (CLIENT_REPOSITORY_MAP.containsKey(id)) {
             client.setId(id);
             CLIENT_REPOSITORY_MAP.put(id, client);
+            log.info("Update client with id {}", id);
             return true;
         }
         return false;
@@ -46,6 +53,7 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public boolean delete(int id) {
+        log.debug("Deleting client with id {}", id);
         return CLIENT_REPOSITORY_MAP.remove(id) != null;
     }
 }
